8d11aa006ef38f581678288cb785bbfff56af3d2
Travis Cook <travismcook93@gmail.com>
2020-11-09 20:37:59 -0700

    fixing bugs

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,9 +1,6 @@
 var express = require("express");
 const burger = require("../models/burger.js");
 var router = express.Router();
-
-var burger = require("../models/burger.js");
-
 router.get("/", function(req, res){
   burger.all(function(data) {
     var handlObj = {
