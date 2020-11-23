057597827ba52237137e540642100783760ce35d
Travis Cook <travismcook93@gmail.com>
2020-11-23 14:52:58 -0700

    db changes

--- models/burger.js
+++ models/burger.js
@@ -2,23 +2,23 @@ const orm = require("../config/orm")
 
 var burger = {
   all: function(cb) {
-    orm.all("Burgers", function(res) {
+    orm.all("burger", function(res) {
       cb(res);
     });
   },
 
   create: function(cols, vals, cb) {
-    orm.create("Burgers", cols, vals, function(res) {
+    orm.create("burger", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("Burgers", objColVals, condition, function(res) {
+    orm.update("burger", objColVals, condition, function(res) {
       cb(res);
     });
   },
   delete: function(table, objColVals, cb) {
-    orm.delete("Burgers", objColVals, function(res){
+    orm.delete("burger", objColVals, function(res){
       cb(res);
     })
   }
